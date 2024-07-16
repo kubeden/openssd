7f80c8165290c4d24831b7f8ddfc49fc9d31cb23
Denislav Gavrilov <denislavgavrilov99@gmail.com>
2024-07-16 16:05:53 +0300

    little push

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 apt update && apt upgrade -y
-sudo apt-get install build-essential procps curl file git -y
+sudo apt-get install build-essential procps curl file git docker.io docker-compose -y
 
 wget https://go.dev/dl/go1.22.5.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.22.5.linux-amd64.tar.gz
 rm go1.22.5.linux-amd64.tar.gz
 echo "export PATH=$PATH:/usr/local/go/bin" > ~/.bashrc
 source ~/.bashrc
-
-apt install docker.io docker-compose -y
 
 mkdir /var/www
 mkdir /var/www/openssd
